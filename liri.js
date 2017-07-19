b85ae801806eb9811e5e36dcb9db73b8b0293c15
Karen <K.Blanchette07@gmail.com>
2017-07-18 19:28:18 -0500

    showTweets Function

--- liri.js
+++ liri.js
@@ -1,13 +1,20 @@
-var twitter = require('twitter');
+var Twitter = require('twitter');
+var keys = require('./keys.js')
 
-var client = new twitter ({
-    consumer_key: process.env.twitterKeys_Consumer_key,
-    consumer_secret: process.env.twitterKeys_consumer_secret,
-    access_token_key: process.env.twitterKeys_access_token_key,
-    access_token_secret: process.env.twitterKeys_access_token_secret
+var client = new Twitter ({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
 });
 
+var params = {
+    screen_name: 'karette_',
+    count: 20
+};
+
 var command = process.argv[2];
+var value = process.argv[3];
 
 switch(command){
     case "my-tweets":
@@ -23,3 +30,18 @@ switch(command){
         action();
         break;
 }
+
+function showTweets(){
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            // console.log(tweets[0].text);
+            tweets.forEach(function(element) {
+                console.log(element.created_at + '\n' + element.text );
+            });
+        }
+        else{
+            console.log('Error!');
+        }
+    });
+}
+
